de4ee963915f57bab6ead86924bb30c5cdb77b60
A1NZ <159454904+Dragbnx@users.noreply.github.com>
2024-05-13 14:30:00 +0200

    modif labyrinthe

--- 0 - Nuit du Code/1 - Labyrinthe/App.py
+++ 0 - Nuit du Code/1 - Labyrinthe/App.py
@@ -1,12 +1,13 @@
-import pyxel
 from Personnage import *
 from Labyrinthe import *
+import pyxel
 BLOC = 8
 HAUT, BAS, GAUCHE, DROITE = (0, -BLOC), (0, BLOC), (-BLOC, 0), (BLOC, 0)
 
 
 class App:
     """ Application graphique - Pyxel """
+
     def __init__(self):
         """ App -> None """
         # initialisation de la fenêtre graphique
@@ -17,21 +18,27 @@ class App:
                           pyxel.KEY_DOWN: BAS,
                           };
         self.lab = Labyrinthe(51,51)
-        pyxel.init(408, 408, title="La Nuit du Code")
-        pyxel.init(128, 128, title="La Nuit du Code")
+        pyxel.init(408, 408, title="La Nuit du Code", fps = 60)
         pyxel.run(self.update, self.draw)
+
+    def exec_btn(self):
+        for cle in self.commandes:
+            if pyxel.btn(cle):
+                self.deplacer(self.commandes[cle])
 
     def update(self):
         """ App -> None
         Met à jour l'application """
+        self.exec_btn()
 
     def draw(self):
         """ App -> None
         Affiche les éléments dans la fenêtre graphique """
+        pyxel.cls(0)
+        self.lab.afficher()
+        self.hero.afficher()
 
-    
-    
-    def deplacer(self, perso, direction):
+    def deplacer(self, direction):
         """ App, Personnage, (int, int) -> None
         Déplace le personnage dans la direction indiquée si cela est possible """
         xp, yp = self.hero.x//BLOC, self.hero.y//BLOC
@@ -45,6 +52,5 @@ class App:
 
 
 
-        pass
 
 App()
--- 0 - Nuit du Code/1 - Labyrinthe/Labyrinthe.py
+++ 0 - Nuit du Code/1 - Labyrinthe/Labyrinthe.py
@@ -1,42 +1,53 @@
+import pyxel
+import random
+
+BLOC = 8
+HAUT, BAS, GAUCHE, DROITE = (0, -BLOC), (0, BLOC), (-BLOC, 0), (BLOC, 0)
+
+
+
 class Labyrinthe:
     """Représente un labyrinthe"""
+
     def __init__(self, n, m):
-        """Labyrinthe, int, int -> None
-        Initialise un labyrinthe vide """
-        assert n%2 == 1 and m%2 == 1
         self.dim = n, m
         self.murs = [[0 for j in range(m)]
                     for i in range(n)]
+        self.generer()
 
     def est_dans(self, bloc):
         """ Labyrinthe, (int, int) -> bool
         Détermine si bloc est un indice de bloc valide pour le labyrinthe self """
-        pass
-    
+        n, m = self.dim
+        x, y = bloc
+        return 0<=x<n and 0<=y<m
+
     def est_sol(self, bloc):
         """ Labyrinthe, (int, int) -> bool
         Détermine si bloc est un indice de bloc valide correspondant à un sol dans le labyrinthe self """
-
         x,y = bloc
-        return self.murs[x][y] != 0
+        return self.murs[x][y] == 1
 
-        pass
-
-    
     def blocs_possibles(self, position, visites):
         """ Labyrinthe, (int, int), {(int, int)} -> [(int, int)]
         Renvoie la liste des blocs voisins et non visités """
-        pass
-    
-    def generer(self, couleur = 1):
+        paul_liste = []
+        x,y = position
+        possibles =[(x, y-2), (x, y+2), (x-2, y), (x+2, y)]
+        for elm in possibles:
+            if self.est_dans(elm) and self.est_sol(elm) and not elm in visites:
+                paul_liste.append(elm)
+        return paul_liste
+
+
+    def generer(self):
         """ Labyrinthe
         Génère un labyrinthe de manière aléatoire """
-
         n, m = self.dim
         for i in range(n):
             for j in range(m):
                 if i%2 == 1 and j%2 == 1:
-                    self.murs[i][j] = couleur
+                    self.murs[i][j] = 1
         visites = set()
         chemin = []
         position_courantes = (1,1)
@@ -52,7 +63,7 @@ class Labyrinthe:
             nouvelle_position = random.choice(choix)
             x, y = position_courantes
             x1, y1 = nouvelle_position
-            self.murs[(x+x1)//2][(y+y1)//2] = couleur
+            self.murs[(x+x1)//2][(y+y1)//2] = 1
             position_courantes = nouvelle_position
             visites.add(position_courantes)
             chemin.append(position_courantes)
@@ -63,13 +74,4 @@ class Labyrinthe:
         n,m = self.dim
         for i in range(n):
             for j in range(m):
-                pyxel.rect(i*8, j*8, 8, 8, self.murs[i][j])
-
-Labyrinthe(15,15)
-        pass
-    
-    def afficher(self):
-        """ Labyrinthe -> None
-        Affiche le labyrinthe self à l'écran """
-        pass
-
+                pyxel.rect(i*8, j*8, 8,8,self.murs[i][j])
